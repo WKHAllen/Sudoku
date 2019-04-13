60ce8c700e366049daa563432168c2d86c838f3d
WKHAllen <wkhallen@gmail.com>
2019-04-13 15:22:34 -0500

    Functional version

--- js/sudoku.js
+++ js/sudoku.js
@@ -1,9 +1,13 @@
-const EMPTY = null;
+const EMPTY = "";
 const NONE_EMPTY = null;
-const NO_SOLUTION = null;
+const NO_SOLUTION = undefined;
 
 function clone(table) {
     return JSON.parse(JSON.stringify(table));
+}
+
+function arraysEqual(arr1, arr2) {
+    return JSON.stringify(arr1) === JSON.stringify(arr2);
 }
 
 function valid(table, number, x, y) {
@@ -14,12 +18,12 @@ function valid(table, number, x, y) {
     for (let arow of table) {
         column.push(arow[y - 1]);
     }
-    if (number in row || number in column) {
+    if (row.includes(number) || column.includes(number)) {
         table[x - 1][y - 1] = current;
         return false;
     }
     let square = [Math.floor((x - 1) / 3), Math.floor((y - 1) / 3)];
-    let values = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    let values = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
     let rows = values[square[0]];
     let cols = values[square[1]];
     for (let i of rows) {
@@ -37,8 +41,8 @@ function valid(table, number, x, y) {
 function validPossibilities(table, x, y) {
     let possibilities = [];
     for (let i = 1; i <= 9; i++) {
-        if (valid(table, i, x, y)) {
-            possibilities.append(i);
+        if (valid(table, i.toString(), x, y)) {
+            possibilities.push(i.toString());
         }
     }
     return possibilities;
@@ -59,11 +63,12 @@ function doSolve(table) {
     let first = firstEmpty(table);
     if (first !== NONE_EMPTY) {
         let possibilities = validPossibilities(table, ...first);
+        console.log(possibilities);
         if (possibilities.length === 0) {
             return NO_SOLUTION;
         }
         for (let value of possibilities) {
-            table[first[0] - 1][first[1] - 1] = value;
+            table[first[0] - 1][first[1] - 1] = value.toString();
             let solution = doSolve(clone(table));
             if (solution !== NO_SOLUTION) {
                 return solution;
@@ -77,8 +82,8 @@ function doSolve(table) {
 function getSolution(table) {
     table = clone(table);
     let tableClone = [];
-    while (table !== tableClone) {
-        let tableClone = clone(table);
+    while (!arraysEqual(table, tableClone)) {
+        tableClone = clone(table);
         for (let i = 1; i <= 9; i++) {
             for (let j = 1; j <= 9; j++) {
                 if (table[i - 1][j - 1] === EMPTY) {
@@ -90,25 +95,133 @@ function getSolution(table) {
             }
         }
     }
-    let solution = doSolve(clone(table));
+    let solution = doSolve(table);
     return solution;
 }
 
+function validTable(table) {
+    for (let i = 1; i <= 9; i++) {
+        for (let j = 1; j <= 9; j++) {
+            if (table[i - 1][j - 1] !== EMPTY) {
+                if (!valid(table, table[i - 1][j - 1], i, j)) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+function flattenTable(table) {
+    newtable = [];
+    for (let i = 0; i < table.length; i++) {
+        for (let j = 0; j < table[i].length; j++) {
+            newtable.push(table[i][j]);
+        }
+    }
+    return newtable;
+}
+
+function unflattenTable(table) {
+    newtable = [[], [], [], [], [], [], [], [], []];
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            newtable[i].push(table[i * 9 + j]);
+        }
+    }
+    return newtable;
+}
+
 function getTable() {
-    // GET THE VALUES IN THE HTML TABLE
+    let table = [];
+    let board = document.getElementById("board");
+    let boardInputs = board.getElementsByTagName("input");
+    for (let element of boardInputs) {
+        table.push(element.value);
+    }
+    return unflattenTable(table);
 }
 
 function setTable(table) {
-    // SET THE VALUES IN THE HTML TABLE
+    table = flattenTable(table);
+    let board = document.getElementById("board");
+    let boardInputs = board.getElementsByTagName("input");
+    for (let i = 0; i < boardInputs.length; i++) {
+        boardInputs[i].value = table[i];
+    }
+}
+
+function showError(message) {
+    let error = document.getElementById("error");
+    let newalert = document.createElement("div");
+    newalert.setAttribute("class", "alert alert-primary");
+    newalert.setAttribute("role", "alert");
+    newalert.innerHTML = message;
+    let newbutton = document.createElement("button");
+    newbutton.setAttribute("type", "button");
+    newbutton.setAttribute("class", "close");
+    newbutton.setAttribute("onclick", "clearError()");
+    let newspan = document.createElement("span");
+    newspan.innerHTML = "&times;";
+    newbutton.appendChild(newspan);
+    newalert.appendChild(newbutton);
+    error.appendChild(newalert);
+}
+
+function clearError() {
+    let error = document.getElementById("error");
+    error.innerHTML = "";
 }
 
 function solve() {
     let table = getTable();
-    let solution = getSolution(table);
-    if (solution === NO_SOLUTION) {
-        // DO SOMETHING WITH THE HTML HERE: SHOW THAT THERE IS NO SOLUTION
+    if (!validTable(table)) {
+        showError("Invalid puzzle");
     } else {
-        setTable(solution);
-        // DO SOMETHING WITH THE HTML HERE: FILL IN THE SOLUTION
+        let solution = getSolution(table);
+        // console.log(solution);
+        if (solution === NO_SOLUTION) {
+            showError("No solution found");
+        } else {
+            clearError();
+            setTable(solution);
+        }
     }
 }
+
+function main() {
+    let board = document.getElementById("board");
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            let newdiv = document.createElement("div");
+            newdiv.classList.add("digit");
+            if (j === 2 || j === 5) {
+                newdiv.classList.add("space-right");
+            } else if (j === 3 || j === 6) {
+                newdiv.classList.add("space-left");
+            }
+            if (i === 2 || i === 5) {
+                newdiv.classList.add("space-below");
+            } else if (i === 3 || i === 6) {
+                newdiv.classList.add("space-above");
+            }
+            let newinput = document.createElement("input");
+            newinput.setAttribute("type", "text");
+            newinput.setAttribute("class", "form-control input");
+            newinput.setAttribute("maxlength", "1");
+            newdiv.appendChild(newinput);
+            board.appendChild(newdiv);
+        }
+    }
+
+    let puzzle = ".27.5.....1....6...9.82....8..7.....5.......4.....2..9....85.6...3....1.....7.98.";
+    puzzle = puzzle.split("");
+    while (puzzle.indexOf(".") !== -1){
+        let index = puzzle.indexOf(".");
+        puzzle[index] = EMPTY;
+    }
+    puzzle = unflattenTable(puzzle);
+    // setTable(puzzle);
+}
+
+window.addEventListener("load", main);
